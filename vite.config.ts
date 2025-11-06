d70300ddab1efcd93b4963dd636c5257e183e53d
sunnieWL <weerapat.16141@gmail.com>
2025-11-06 14:10:22 +0700

    Fix to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-// filepath: demo-se2-frontend/vitest.config.ts
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+// https://vitejs.dev/config/
 export default defineConfig({
- test: {
- environment: 'jsdom',
- globals: true,
- },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
